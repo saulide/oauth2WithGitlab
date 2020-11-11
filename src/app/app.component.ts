6b9ed7b98fdea5f6e8e9223980a4dd616440bf3e
Saulius Archipovas <saulius.archipovas@mevis.fraunhofer.de>
2020-11-11 14:46:28 +0100

    Added oauth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthConfig, OAuthService} from 'angular-oauth2-oidc';
+
+const authConfig: AuthConfig = {
+  issuer: 'https://gitlab.com',
+  clientId: 'bd488d42573ef10f465e96f18df01b6895046721bb6c3cb5be86d5817fd2f2b5',
+  redirectUri: window.location.origin,
+  responseType: 'code',
+  scope: 'profile email api read_repository',
+  showDebugInformation: true
+};
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'openid';
+
+  constructor(private oauthService: OAuthService) {
+    this.oauthService.configure(authConfig);
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  onLogin = () => {
+    this.oauthService.initCodeFlow();
+  }
 }
